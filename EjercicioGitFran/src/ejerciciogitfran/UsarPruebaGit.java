56d256c3270884276552bb19275e14c3f4e8acbd
Fran <unocualquiera@mail.com>
2020-05-08 12:49:14 +0200

    Añadida clase UsarPruebaGit

---
+++ EjercicioGitFran/src/ejerciciogitfran/UsarPruebaGit.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejerciciogitfran;
+
+/**
+ *
+ * @author unomi
+ */
+public class UsarPruebaGit {
+    public static void main(String[] args) {
+        System.out.println("Hola mundo desde git");
+        PruebaGit prueba = new PruebaGit();
+        prueba.num=20;
+        System.out.println(prueba.num);
+    }
+}
